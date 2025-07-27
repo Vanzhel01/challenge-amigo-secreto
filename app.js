e832d0a6bb08c6056cbf27fa514fbe8e8359a019
Luigui Huanuco <huanucoluigui@gmail.com>
2025-07-27 12:43:47 -0500

    Creacion de la funcion agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // Creacion de la Lista amigos
 let listaAmigos=[];
+// Function para agregar amigos
+function agregarAmigo(){
+    //guarda el dato del elemento html en la variable amigo
+    var amigo=document.getElementById("amigo").value;
+    //condicional para verificar que es un dato valido y no uno en blanco
+    if(amigo!=""){
+        listaAmigos.push(amigo);
+        
+    }
+    else{
+        alert("Por favor inserte un nombre");
+    }  
+    //console.log(listaAmigos);
+}
+
